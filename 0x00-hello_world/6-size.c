d15ff6c8511a6de7cec6155802b2e1701b7658fd
ketchy lewis <ketchylewis@gmail.com>
2022-06-30 16:11:28 -0700

    TASK 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-/*
+/**
 * main - prints the size of various types
 * Description: prints "\"Programming is like building a multilingual puzzle"
 * Return: 0 if exited properly, non-zero otherwise
